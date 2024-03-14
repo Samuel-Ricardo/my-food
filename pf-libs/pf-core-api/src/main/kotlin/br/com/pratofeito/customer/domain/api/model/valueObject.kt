0859cc9b584a4495874ae7918c6b05edf821e46a
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-03-13 22:11:47 -0300

    [ :sparkles: ] | create: value object - comsumer id (custommer::domain)

---
+++ pf-libs/pf-core-api/src/main/kotlin/br/com/pratofeito/customer/domain/api/model/valueObject.kt
@@ -0,0 +1,10 @@
+package br.com.pratofeito.customer.domain.api.model
+
+import java.io.Serializable
+import java.util.UUID
+
+data class CustomerId(val identifier:String): Serializable {
+    constructor(): this(UUID.randomUUID().toString());
+
+    override fun toString(): String = identifier;
+}
