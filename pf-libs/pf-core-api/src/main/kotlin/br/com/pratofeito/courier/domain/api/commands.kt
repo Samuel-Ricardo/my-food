20658e66d1787fc1d65666fdfb7348c486e04fa7
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-04-03 21:46:31 -0300

    feat(courier-domain-api): implements courier domain api [valueObjects, commands, events]

---
+++ pf-libs/pf-core-api/src/main/kotlin/br/com/pratofeito/courier/domain/api/commands.kt
@@ -0,0 +1,57 @@
+package br.com.pratofeito.courier.domain.api
+
+import br.com.pratofeito.common.domain.api.AuditableAbstractCommand
+import br.com.pratofeito.common.domain.api.model.AuditEntry
+import br.com.pratofeito.common.domain.api.model.PersonName
+import br.com.pratofeito.courier.domain.api.model.CourierId
+import br.com.pratofeito.courier.domain.api.model.CourierOrderId
+import org.axonframework.modelling.command.TargetAggregateIdentifier
+import javax.validation.Valid
+
+abstract class CourierCommand(
+    open val targetAggregateIdentifier: CourierId,
+    override val auditEntry: AuditEntry
+): AuditableAbstractCommand(auditEntry)
+
+abstract class CourierOrderCommand(
+    open val targetAggregateIdentifier: CourierOrderId,
+    override val auditEntry: AuditEntry
+): AuditableAbstractCommand(auditEntry)
+
+data class CreateCourierCommand(
+    @TargetAggregateIdentifier
+    override val targetAggregateIdentifier: CourierId,
+    @field:Valid val name: PersonName,
+    val maxNumberOfActiveOrders: Int,
+    override val auditEntry: AuditEntry
+): CourierCommand(targetAggregateIdentifier, auditEntry) {
+
+    constructor(
+        name: PersonName,
+        maxNumberOfActiveOrders: Int,
+        auditEntry: AuditEntry
+    ): this(CourierId(), name, maxNumberOfActiveOrders, auditEntry)
+
+}
+
+data class CreateCourierOrderCommand(
+    @TargetAggregateIdentifier
+    override val targetAggregateIdentifier: CourierOrderId,
+    override val auditEntry: AuditEntry
+) : CourierOrderCommand(targetAggregateIdentifier, auditEntry) {
+
+    constructor( auditEntry: AuditEntry ): this(CourierOrderId(), auditEntry)
+}
+
+data class AssignCourierOrderCommand(
+    @TargetAggregateIdentifier
+    override val targetAggregateIdentifier: CourierOrderId,
+    val courierId: CourierId,
+    override val auditEntry: AuditEntry
+): CourierOrderCommand(targetAggregateIdentifier, auditEntry)
+
+data class MarkCourierOrderAsDeliveredCommand(
+    @TargetAggregateIdentifier
+    override val targetAggregateIdentifier: CourierOrderId,
+    override val auditEntry: AuditEntry
+) : CourierOrderCommand(targetAggregateIdentifier, auditEntry)
---
+++ pf-libs/pf-core-api/src/main/kotlin/br/com/pratofeito/courier/domain/api/model/valueObject.kt
@@ -0,0 +1,22 @@
+package br.com.pratofeito.courier.domain.api.model
+
+import java.util.UUID
+
+
+enum class CourierOrderState {
+    CREATED, ASSIGN_PENDIGN, ASSIGNED, DELIVERED, CANCEL_PENDING, CANCELED
+}
+
+data class CourierId(val identifier: String) {
+
+    constructor(): this(UUID.randomUUID().toString());
+
+    override fun toString(): String = identifier;
+}
+
+data class CourierOrderId(val identifier: String) {
+
+    constructor(): this(UUID.randomUUID().toString());
+
+    override fun toString(): String = identifier;
+}
